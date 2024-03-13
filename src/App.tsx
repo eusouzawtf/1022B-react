de6243f1a6f30d8efd540ca6779aa81d286071e4
sh0uwzz <luis.ribeiro2@estudante.ifms.edu.br>
2024-03-13 10:15:02 -0400

    Meu Primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import MeuComponente from './componentes/MeuComponente'
 function App() {
   let nome = "Luís Guilherme"
   let sobrenome = "Souza"
   return (
     <>
+    <MeuComponente></MeuComponente>
       <h1 className="nome">
         Nome: {nome}
         </h1>
---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,16 @@
+import './MeuComponente.css'
+
+//Criar nosso primeiro componente.
+function MeuComponente(){
+    return (
+        <header className='header'>
+            <div className='logo'>
+                {variavel}
+            </div>
+        </header>
+    )
+}
+
+
+
+export default MeuComponente;
